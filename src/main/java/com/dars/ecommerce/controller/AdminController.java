6b59b68ddec66a9a7d5e626f573377f55fdab2b2
Darshan L <darshanl1254@gmail.com>
2024-10-28 23:34:20 +0530

    Admin Products Approve and Reject

--- src/main/java/com/dars/ecommerce/controller/AdminController.java
+++ src/main/java/com/dars/ecommerce/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dars.ecommerce.dto.Product;
@@ -33,7 +34,7 @@ public class AdminController {
 	@GetMapping("/products")
 	public String displayProducts(HttpSession session, ModelMap map) {
 		if (session.getAttribute("admin") != null) {
-			
+
 			List<Product> products = productRepository.findAll();
 			if (products.isEmpty()) {
 				session.setAttribute("failure", "No Products Added Yet");
@@ -42,11 +43,42 @@ public class AdminController {
 				map.put("products", products);
 				return "admin-products.html";
 			}
-			
+
 		} else {
 			session.setAttribute("failure", "Invalid Session, Login Again");
 			return "redirect:/login";
 		}
 	}
 
+	@GetMapping("/approve-product/{id}")
+	public String approveProduct(HttpSession session, @PathVariable int id) {
+		if (session.getAttribute("admin") != null) {
+
+			Product product = productRepository.findById(id).orElseThrow();
+			product.setApproved(true);
+			productRepository.save(product);
+			session.setAttribute("success", "Product Approved Successfully");
+			return "redirect:/admin/products";
+
+		} else {
+			session.setAttribute("failure", "Invalid Session, Login Again");
+			return "redirect:/login";
+		}
+	}
+
+	@GetMapping("/reject-product/{id}")
+	public String rejectProduct(HttpSession session, @PathVariable int id) {
+		if (session.getAttribute("admin") != null) {
+
+			Product product = productRepository.findById(id).orElseThrow();
+			product.setApproved(false);
+			productRepository.save(product);
+			session.setAttribute("failure", "Product Reject Successfully");
+			return "redirect:/admin/products";
+
+		} else {
+			session.setAttribute("failure", "Invalid Session, Login Again");
+			return "redirect:/login";
+		}
+	}
 }
